0616f7c0ece4fb9c67809c1a5ca364b7e064e50a
Ernesto Lowy <ernesto@ebi.ac.uk>
2017-08-16 10:15:18 +0100

    adding SNPTools modifications
    Former-commit-id: 61e10ae14d84993ab87db6e12d096ab9a1ed39e7

--- VARIANT_CALLING/Hive/PyHive/VcfGenotype/SNPTools_prob2vcf.py
+++ VARIANT_CALLING/Hive/PyHive/VcfGenotype/SNPTools_prob2vcf.py
@@ -9,11 +9,17 @@ class SNPTools_prob2vcf(eHive.BaseRunnable):
         self.warning("Analysing file {0}".format(self.param_required('vcf_file')))
 
         vcf_g=VcfGenotype(vcf=self.param_required('vcf_file'),snptools_folder=self.param_required('snptools_folder'))
+        chro=self.param_required('chr').rstrip('\n')
 
         outprefix=self.param_required('outprefix')
         
-        vcf_f=vcf_g.run_snptools_prob2vcf(probf=self.param_required('probf'),outprefix=outprefix+"_{0}".format(self.param_required('chr')),
-                                          chro=self.param_required('chr'),outdir=self.param_required('work_dir'))
+        vcf_f=""
+        if self.param_is_defined('verbose'):
+            vcf_f=vcf_g.run_snptools_prob2vcf(probf=self.param_required('probf'),outprefix=outprefix+"_{0}".format(chro),
+                                              chro=self.param_required('chr'),outdir=self.param_required('work_dir'), verbose=True)
+        else:
+            vcf_f=vcf_g.run_snptools_prob2vcf(probf=self.param_required('probf'),outprefix=outprefix+"_{0}".format(chro),
+                                              chro=self.param_required('chr'),outdir=self.param_required('work_dir'), verbose=False)
        
         self.param('vcf_f', vcf_f)
 
