afffdd2b3b06dc1ff9a8e1d31b6b505cd87e4ffc
Ernesto Lowy <ernesto@ebi.ac.uk>
2017-09-05 12:40:56 +0100

    adding new BAMQC tests

--- VARIANT_CALLING/BamQC/tests/conftest.py
+++ VARIANT_CALLING/BamQC/tests/conftest.py
@@ -1,7 +1,7 @@
 import pytest
 
 def pytest_addoption(parser):
-    parser.addoption('--bam', default='data/input.test.new.bam' ,action='store_true', help='Path to test BAM file')
+    parser.addoption('--bam', default='data/exampleBAM.bam' ,action='store_true', help='Path to test BAM file')
     parser.addoption('--chk_indel_folder', default='~/bin/' ,action='store_true', help='Folder with chk_indel_rg binary')
     parser.addoption('--samtools_folder', default='/Users/ernesto/bin/anaconda/envs/python3/bin/',
                      action='store_true', help='Folder with samtools binary')
