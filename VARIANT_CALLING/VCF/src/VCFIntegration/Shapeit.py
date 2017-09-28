47d349e911d2a9ebf3a66dbbf3aa94d39c85bbde
Ernesto Lowy <ernesto@ebi.ac.uk>
2017-09-28 13:57:24 +0100

    adding script to correct ambiguous bases

--- VARIANT_CALLING/VCF/src/VCFIntegration/Shapeit.py
+++ VARIANT_CALLING/VCF/src/VCFIntegration/Shapeit.py
@@ -23,30 +23,49 @@ class Shapeit(object):
         '''
         self.shapeit_folder = shapeit_folder
 
-    def run_shapeit(self, input_gen, input_init, input_scaffold, input_map, output_prefix, from=None, to=None, **kwargs):
+    def run_shapeit(self, input_gen, input_init, input_scaffold, output_prefix, input_map=None,
+                    verbose=False, **kwargs):
         '''
         Run Shapeit
         
         Parameters
         ----------
-        input_gen : Tuple, Required
-                    specifies the genotype/GL input data that you obtain from Beagle4, i.e. ('input.shapeit.20.gen.gz','input.shapeit.20.gen.sample')
-        input_init : Tuple, Required
-                     specifies the haplotypes that you obtain from Beagle4, i.e. ('input.shapeit.20.hap.gz','input.shapeit.20.hap.sample')
-        input_scaffold : Tuple, Required
-                         SNP-array derived haplotype scaffold used by SHAPEIT. It has to be in Impute2 format. i.e. ('scaffold.haps.gz','scaffold.haps.sample')
-        input_map : str, Required
-                    Path with the genetic map
+        input_gen : str, Required
+                    specifies the genotype/GL input data that you obtain from Beagle4, i.e. 'input.shapeit.20.gen.gz input.shapeit.20.gen.sample'
+        input_init : str, Required
+                     specifies the haplotypes that you obtain from Beagle4, i.e. 'input.shapeit.20.hap.gz input.shapeit.20.hap.sample'
+        input_scaffold : str, Required
+                         SNP-array derived haplotype scaffold used by SHAPEIT. It has to be in Impute2 format. i.e. 'scaffold.haps.gz scaffold.haps.sample'
         output_prefix : str, Required
-                        Prefix used for the 2 output files estimated by SHAPEIT, i.e. 'output.shapeit.20.haps.gz output.shapeit.20.haps.sample',
-        from : int, Optional
+                        Prefix used for the 2 output files estimated by SHAPEIT, i.e. 'output.shapeit.20.haps.gz output.shapeit.20.haps.sample'
+        input_map : str, Optional
+                    Path with the genetic map
+        i_from : int, Optional
                specify the region to be phased
-        to : int, Optional
+        i_to : int, Optional
              specify the region to be phased
+        verbose : bool, optional
+                  if true, then print the command line used for running this program
         
         '''
+
+        
 
         command = ""
 
         if self.shapeit_folder:
             command += self.shapeit_folder+"/"
+
+        command += "shapeit -call --input-gen {0} --input-init {1} --input-scaffold {2} --output-max {3}.haps.gz {3}.haps.sample --output-log {3}.log".format(input_gen, input_init, input_scaffold,
+                                                                                                                                                              output_prefix)
+        for k,v in kwargs.items():
+            command += " --{0} {1}".format(k,v)
+
+        if verbose==True:
+            print("Command used was: %s" % command)
+             
+        try:
+            subprocess.check_output(command, shell=True)
+        except subprocess.CalledProcessError as exc:
+            print("Command used was: {0}".format(command))
+            raise Exception(exc.output)
