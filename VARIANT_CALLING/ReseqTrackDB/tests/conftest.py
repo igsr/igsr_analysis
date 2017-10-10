0ca24827db183ed62dada0882699cac3b9adf5f3
Ernesto Lowy <ernesto@ebi.ac.uk>
2017-10-10 10:39:51 +0100

    adding rename function

---
+++ VARIANT_CALLING/ReseqTrackDB/tests/conftest.py
@@ -0,0 +1,9 @@
+import pytest
+
+def pytest_addoption(parser):
+    parser.addoption('--hostname', default='localhost', action='store_true', help='host name')
+    parser.addoption('--username', default='g1krw', action='store_true', help='user name')
+    parser.addoption('--port', default=4197, action='store_true', help='port')
+    parser.addoption('--pwd', default='test', action='store_true', help='password')
+    parser.addoption('--db', default='g1k_archive_staging_track', action='store_true', help='database name')
+
