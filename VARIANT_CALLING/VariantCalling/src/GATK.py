caafc65b8ef6ca238416d3f1ff9deef190de045f
Ernesto Lowy <ernesto@ebi.ac.uk>
2018-02-23 11:45:58 +0000

    GATK UG wrapper
    Former-commit-id: 509e9aa250cde99b9d6c673ed438dba7ad69a4fb

---
+++ VARIANT_CALLING/VariantCalling/src/GATK.py
@@ -0,0 +1,96 @@
+'''
+Created on 22 Feb 2018
+
+@author: ernesto
+'''
+
+import os
+import subprocess
+import pdb
+
+class GATK(object):
+    '''
+    Class to run the different GATK variant callers
+    '''
+
+
+    def __init__(self, bam, reference, gatk_folder, bgzip_folder=None):
+        '''
+        Constructor
+
+        Class variables
+        ---------------
+        bam : str, Required
+             Path to BAM file used for the variant calling process
+        reference : str, Required
+             Path to fasta file containing the reference
+        gatk_folder : str, Required
+                       Path to folder containing GATK's jar file
+        bgzip_folder : str, Optional
+                       Path to folder containing the bgzip binary
+        '''
+
+        if os.path.isfile(bam) is False:
+            raise Exception("BAM file does not exist")
+
+        self.bam = bam
+        self.reference = reference
+        self.gatk_folder = gatk_folder
+        self.bgzip_folder = bgzip_folder
+
+    def run_ug(self, outprefix, glm='SNP', compress=True, nt=1, intervals=None, verbose=None, **kwargs):
+        
+        '''
+        Run GATK UnifiedGenotyper
+
+        Parameters
+        ----------
+        outprefix : str, Required
+                    Prefix for output VCF file. i.e. /path/to/file/test
+        glm : str, Required
+              Genotype likelihoods calculation model to employ -- SNP is the default option, 
+              while INDEL is also available for calling indels and BOTH is available for
+              calling both together
+        compress : boolean, Default= True
+                   Compress the output VCF
+        nt : int, Optional
+             Number of data threads to allocate to UG
+        intervals : str, Optional
+                    Path to file with genomic intervals to operate with
+        verbose : bool, optional
+                  if true, then print the command line used for running this program
+
+        Returns
+        -------
+        A VCF file
+
+        '''
+
+        command = "java -jar "
+        if self.gatk_folder:
+            command += self.gatk_folder + "/"
+
+        command += "GenomeAnalysisTK.jar -T UnifiedGenotyper -R {0} -I {1} " \
+                   "-glm {2} ".format(self.reference, self.bam, glm)
+
+        if intervals:
+            command += " -L {0}".format(intervals)
+
+        for k,v in kwargs.items():
+            command += " --{0} {1}".format(k,v)
+
+        if compress is True:
+            outprefix += ".vcf.gz"
+            command += "| {0}/bgzip -c > {1}".format(self.bgzip_folder,outprefix)
+
+        else:
+            outprefix += ".vcf"
+            command += " -o {0}".format(outprefix)
+
+        try:
+            subprocess.check_output(command, shell=True)
+        except subprocess.CalledProcessError as exp:
+            print("Something went wrong while running GATK UG")
+            raise Exception(exp.output)
+
+        return outprefix
