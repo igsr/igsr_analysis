4abfb92b914703b9f694b62c3ac3d2b8edd39bdc
Ernesto Lowy <ernesto@ebi.ac.uk>
2018-04-18 16:52:31 +0100

    deleting VARIANT_CALLING

--- VARIANT_CALLING/VariantCalling/tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-def pytest_addoption(parser):
-    parser.addoption('--bam', default='data/exampleBAM.bam', action='store_true', help='Path to test BAM file')
-    parser.addoption('--reference', default='data/exampleFASTA.fasta', action='store_true', help='Path to Fasta file')
-    parser.addoption('--gatk_folder', default='~/bin/GATK/', action='store_true', help='Path to folder containing the GATK jar file')
-    parser.addoption('--bgzip_folder', default='/nfs/production/reseq-info/work/ernesto/bin/anaconda3/bin/', action='store_true', help='Path to folder containing the Bgzip binary')
-
-
