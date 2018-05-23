9ee2d76ef174a0982b4988e83119acf313ec9883
Ernesto Lowy <ernesto@ebi.ac.uk>
2018-05-23 13:32:02 +0100

    modifying ehive SelectVariants wrapper and adding the uncalled option
    Former-commit-id: 4be7718dcf3f1b47072e81b6931015f6fe675d99

--- PyHive/PipeConfig/INTEGRATION/VCFIntegrationGATKSNP.pm
+++ PyHive/PipeConfig/INTEGRATION/VCFIntegrationGATKSNP.pm
@@ -533,6 +533,7 @@ sub pipeline_analyses {
             -language   => 'python3',
             -parameters => {
                 'filepath' => '#vcf_filt#',
+		'uncalled' => 'exclude', #it is necessary to exclude sites with missing genotypes in order for Beagle not to crash
                 'outprefix' => '#vcf_filt#',
                 'work_dir' => $self->o('work_dir'),
                 'bcftools_folder' => $self->o('bcftools_folder')
