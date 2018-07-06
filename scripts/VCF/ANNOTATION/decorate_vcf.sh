8dba20353123cca0751526024ec1f4b4e110781e
Ernesto Lowy <ernesto@ebi.ac.uk>
2018-07-06 10:14:32 +0100

    adding last changes to decorator code
    Former-commit-id: 8420f93587e193a75df3281839d0c05e9e0190a4

--- scripts/VCF/ANNOTATION/decorate_vcf.sh
+++ scripts/VCF/ANNOTATION/decorate_vcf.sh
@@ -17,41 +17,48 @@ region=`echo $3 |sed s/:/./g`
 
 #create folder for annotation table
 mkdir ./ann_table
-#rm -f ./ann_table/*
+rm -f ./ann_table/*
 
 #create the annotation table
 cmd1="python $IGSR_ROOT/scripts/VCF/ANNOTATION/annotate.py --AFcalc $IGSR_ROOT/scripts/VCF/ANNOTATION/ --phased_vcf $1 --sample_panel $IGSR_ROOT/SUPPORTING/integrated_allsamples.20180619.superpopulations.panel --tabix $6 --region $3 --pops $4 --exome $5 --outdir ./ann_table --ann_vcf $2"
 echo $cmd1
 echo "[INFO] running Python annotate.py"
-#$cmd1
+$cmd1
 echo "[INFO] running Python annotate.py-DONE"
 
-#echo "[INFO] compression annotation table"
-#cmd2="bgzip ann_table/annot_tab2.${region}.txt"
-#$cmd2
-#echo "[INFO] compression annotation table-DONE"
+echo "[INFO] compression annotation table"
+cmd2="bgzip ann_table/annot_tab2.${region}.txt"
+$cmd2
+echo "[INFO] compression annotation table-DONE"
 
-#echo "[INFO] running tabix on the annotation table"
-#cmd3="tabix -f -s1 -b2 -e3 ann_table/annot_tab2.${region}.txt.gz"
-#$cmd3
-#echo "[INFO] running tabix on the annotation table-DONE"
+echo "[INFO] running tabix on the annotation table"
+cmd3="tabix -f -s1 -b2 -e3 ann_table/annot_tab2.${region}.txt.gz"
+$cmd3
+echo "[INFO] running tabix on the annotation table-DONE"
 
 #decorate the VCF and also change chromosome names
-#echo "[INFO] running bcftools annotate"
-#dec_out=$(basename -s .vcf.gz $1).$region".decorated.vcf.gz"
-#cmd4="bcftools annotate -r $3 -a ann_table/annot_tab2.${region}.txt.gz -h $IGSR_ROOT/SUPPORTING/annots_26062018.txt --rename-chrs $IGSR_ROOT/SUPPORTING/ensembl2ucsc_chrdict.txt -c CHROM,FROM,TO,REF,ALT,DP,AN,AC,AF,EAS_AF,EUR_AF,AFR_AF,AMR_AF,SAS_AF,EX_TARGET,VT,NS $1 -o $dec_out -Oz"
-#$cmd4
-#echo "[INFO] running bcftools annotate-DONE"
+echo "[INFO] running bcftools annotate"
+dec_out=$(basename -s .vcf.gz $1).$region".decorated.vcf.gz"
+cmd4="bcftools annotate -r $3 -a ann_table/annot_tab2.${region}.txt.gz -h $IGSR_ROOT/SUPPORTING/annots_26062018.txt --rename-chrs $IGSR_ROOT/SUPPORTING/ensembl2ucsc_chrdict.txt -c CHROM,FROM,TO,REF,ALT,DP,AN,AC,AF,EAS_AF,EUR_AF,AFR_AF,AMR_AF,SAS_AF,EX_TARGET,VT,NS $1 -o $dec_out -Oz"
+$cmd4
+echo "[INFO] running bcftools annotate-DONE"
 
 #Now, modify the header on the decorated VCF
-#echo "[INFO] running bcftools reheader"
-#reheaded_out=$(basename -s .vcf.gz $dec_out)".reheaded.vcf.gz"
-#cmd5="bcftools reheader -h $IGSR_ROOT/SUPPORTING/header_26062018.txt $dec_out -o $reheaded_out"
-#$cmd5
-#echo "[INFO] running bcftools reheader-DONE"
+echo "[INFO] running bcftools reheader"
+reheaded_out=$(basename -s .vcf.gz $dec_out)".reheaded.vcf.gz"
+cmd5="bcftools reheader -h $IGSR_ROOT/SUPPORTING/header_26062018.txt $dec_out -o $reheaded_out"
+$cmd5
+echo "[INFO] running bcftools reheader-DONE"
+
+#Recompressing to prevent last line errors in the validator
+echo "[INFO] recompressing"
+`zcat $reheaded_out |bgzip -c > tmp.vcf.gz`
+cmd7="mv tmp.vcf.gz $reheaded_out"
+$cmd7
+echo "[INFO] recompressing-DONE"
 
 #validate the VCF
-#echo "[INFO] running vcf validator"
-#cmd6="vcf_validator_linux -i $reheaded_out" 
-#$cmd6
-#echo "[INFO] running vcf validator-DONE"
+echo "[INFO] running vcf validator"
+cmd8="vcf_validator_linux -i $reheaded_out" 
+$cmd8
+echo "[INFO] running vcf validator-DONE"
