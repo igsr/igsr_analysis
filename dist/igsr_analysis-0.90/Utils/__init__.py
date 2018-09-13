f43972dbc72bdeb697c9481cf1f6d10c75262f60
Ernesto Lowy <ernesto@ebi.ac.uk>
2018-09-13 11:06:56 +0100

    removing previous version
    Former-commit-id: 4d78509ca35fa9a86502400a44b79f2836a58767

--- dist/igsr_analysis-0.90/Utils/__init__.py
+++
@@ -1,1 +0,0 @@
-from Utils.RunProgram import RunProgram
--- dist/igsr_analysis-0.90/VCF/VCFIntegration/__init__.py
+++
@@ -1,3 +0,0 @@
-from VCF.VCFIntegration.Beagle import Beagle
-from VCF.VCFIntegration.Shapeit import Shapeit
-from VCF.VCFIntegration.SNPTools import SNPTools
