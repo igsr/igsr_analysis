6b00e635dcdfb6f9f7d9382dfd19614adcbc8c08
Ernesto Lowy <ernestolowy@gmail.com>
2018-11-06 11:06:06 +0000

    adding analysis for excluding the uncalled genotypes
    Former-commit-id: 74669324ef01bbbc160ebb429c03b73f1861c740

---
+++ tests/test_env.sh
@@ -0,0 +1,5 @@
+export PATH=/homes/ernesto/lib/ensembl-hive_2.4/scripts/:${PATH}
+export PYTHONPATH=/homes/ernesto/lib/ensembl-hive_2.4/wrappers/python3/
+
+export PERL5LIB=""
+export PERL5LIB=/nfs/production/reseq-info/work/ernesto/isgr/VARIANT_CALLING/VARCALL_ALLGENOME_13022017/COMBINING/DEVEL/NEWPHASING/lib/igsr_analysis/:/homes/ernesto/lib/ensembl-hive_2.4/modules/
