c75e7fd129aaad4d2df66cda6fcf0c8b83e33d76
Ernesto Lowy <ernestolowy@gmail.com>
2019-02-14 12:44:07 +0000

    encapsulating code for RFE
    Former-commit-id: 5d7d48bf01f90bf363e29b9edfeb651c277c02d8

--- tests/test_MLclassifier.py
+++ tests/test_MLclassifier.py
@@ -2,6 +2,7 @@ import os
 import warnings
 import pytest
 import glob
+import pdb
 
 from VCF.VCFfilter.MLclassifier import MLclassifier
 
@@ -47,7 +48,7 @@ def test_train_indels():
 
     assert os.path.isfile(outfile) is True
 
-def test_apply_model(clean_tmp):
+def test_apply_model():
 
     ML_obj=MLclassifier(bcftools_folder = pytest.config.getoption('bcftools_folder'),
                         fitted_model = 'data/outdir/fitted_logreg_snps.sav')
@@ -58,3 +59,11 @@ def test_apply_model(clean_tmp):
 
     assert os.path.isfile(outfile) is True
 
+def test_rfe(clean_tmp):
+
+     ML_obj=MLclassifier(bcftools_folder = pytest.config.getoption('bcftools_folder'))
+
+     select_feats=ML_obj.rfe(tp_annotations=pytest.config.getoption('--tp_annotations_indels'),
+                             fp_annotations=pytest.config.getoption('--fp_annotations_indels'), n_features=5)
+     
+     assert all([a == b for a, b in zip(select_feats, ['[4]IDV', '[5]IMF', '[6]VDB', '[7]SGB', '[11]HOB'])])
