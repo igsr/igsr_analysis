c35cb3eeee0e8014d00f94933a7c76427d74547a
Ernesto Lowy <ernestolowy@gmail.com>
2019-07-16 11:29:18 +0100

    Modification in script to generate a download manifest from index
    Former-commit-id: c8a4a6bdfbb0c458fecd88b31515f373a1594e1c

--- scripts/FILE/get_manifest_from_index.py
+++ scripts/FILE/get_manifest_from_index.py
@@ -2,13 +2,12 @@
 This script will generate a manifest file (that can be used with nextflow download pipeline)
 from an index file
 
-usage: print get_manifest_from_index.py <aln.index> </path/to/download/>
+usage: print get_manifest_from_index.py <aln.index>
 """
-
 import sys
 import os
 
-print("url,dest")
+print("url,name")
 
 with open(sys.argv[1]) as f:
  for line in f:
@@ -17,5 +16,4 @@ with open(sys.argv[1]) as f:
          elms=line.split("\t")
          url=elms[0]
          basename=os.path.basename(url)
-         dest="{0}/{1}".format(sys.argv[2],basename)
-         print("{0},{1}".format(url,dest))
+         print("{0},{1}".format(url,basename))
