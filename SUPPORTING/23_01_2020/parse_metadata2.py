cb82403c060f49ea3306e0830834671e36d97c5e
Ernesto Lowy <ernestolowy@gmail.com>
2021-05-20 10:56:10 +0100

    Delete SUPPORTING dir, as it has moved to reseq-personal repo

--- SUPPORTING/23_01_2020/parse_metadata2.py
+++
@@ -1,8 +0,0 @@
-with open("ggvp_samples_metadata.f2.txt") as f:
- for line in f:
-  line=line.rstrip('\n')
-  elms=line.split("\t")
-  if elms[2]=="RELATED":
-   print(elms[0]+"\tCHILD")
-  else:
-   print(elms[0]+"\t"+elms[1])
