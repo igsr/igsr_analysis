da19111131bd2a62e8358ee503a39ae5ae6075f4
Ernesto Lowy <ernestolowy@gmail.com>
2021-09-22 18:07:52 +0100

    changes in vgtoolkit

--- GRAPH/VgToolkit.py
+++ GRAPH/VgToolkit.py
@@ -4,11 +4,13 @@ Created on 14 Sep 2021
 @author: ernesto lowy ernestolowy@gmail.com
 '''
 import pdb
+import glob
 
 from Utils.RunProgram import RunProgram
+from collections import namedtuple
 
 class VG(object):
-     """
+    """
     Class to run the different programs within the vg-toolkit
     (https://github.com/vgteam/vg)
 
@@ -19,24 +21,151 @@ class VG(object):
     """
     vg_folder = None 
 
-    def __init__(self, bam):
+    def __init__(self):
+        pass
+    
+    def run_autoindex(self, ref: str, vcf: str, prefix: str, verbose: bool=False):
         """
-        Constructor
+        run vg autoindex
 
         Parameters
         ----------
-        bam : str
-             Path to BAM file used for the variant calling process.
+        ref : str
+              FASTA file containing the reference sequence
+        vcf : str
+              VCF file with sequence names matching -r
+        prefix : str
+                 Output prefix
+        verbose : bool,  default=False
+                  if true, then print the command line used for running this program
+
+        Returns
+        -------
+        outfiles : list
+                   List of output files
         """
+        Arg = namedtuple('Argument', 'option value')
+        args = [Arg('--workflow', 'giraffe')]
+        args.append(Arg('-r', ref))
+        args.append(Arg('-v', vcf))
+        args.append(Arg('-p', prefix))
 
-        if os.path.isfile(bam) is False:
-            raise Exception("BAM file does not exist")
+        program_cmd= f"{VG.vg_folder}/vg autoindex" if VG.vg_folder else "vg autoindex"
+        vg_runner = RunProgram(program=program_cmd,
+                               args=args)
+        
+        if verbose is True:
+            print("Command line is: {0}".format(vg_runner.cmd_line))
 
-        self.bam = bam
-     
-    def run_giraffe(self):
+        stdout, stderr, is_error = vg_runner.run_popen(raise_exc=False)
+
+        outfiles = glob.glob(f"{prefix}*")
+        
+        return outfiles
+
+    def run_giraffe(self, gbz_f: str, min: str, dist: str, fastq: str, prefix: str, verbose: bool= False ) -> str:
+        """
+        run vg giraffe
+
+        Parameters
+        ----------
+        gbz_f : str
+               path to GBZ file (GBWT index + GBWTGraph)
+        min : str
+             path to minimizer index file
+        dist : str
+              path to the distance index file
+        fastq : str
+                path to FASTQ file
+        prefix : str
+                 Output prefix
+        verbose : bool,  default=False
+                  if true, then print the command line used for running this program
+        
+        Returns
+        -------
+        gam file : str
+                   Path to gam file
+        """
+        Arg = namedtuple('Argument', 'option value')
+        args = (Arg('-Z', gbz_f), Arg('-m', min), Arg('-d', dist), Arg('-f', fastq), Arg('>', f"{prefix}.gam"))
 
         program_cmd= f"{VG.vg_folder}/vg giraffe" if VG.vg_folder else "vg giraffe"
 
-        runner = RunProgram(program=program_cmd,
-                            parameters=['-h'])
+        vg_runner = RunProgram(program=program_cmd,
+                               args=args)
+        
+        if verbose is True:
+            print("Command line is: {0}".format(vg_runner.cmd_line))
+
+        stdout, stderr, is_error = vg_runner.run_popen(raise_exc=False)
+
+        return f"{prefix}.gam"
+ 
+    def run_stats(self, aln_f: str, verbose: bool= False ) -> str:
+        """
+        run vg stats
+
+        Parameters
+        ----------
+        aln_f : str
+               path to alignment file
+        
+        Returns
+        -------
+        stats_f : str
+                  Path to file containing stats on the alingment
+        """
+        Arg = namedtuple('Argument', 'option value')
+        args = (Arg('-a', aln_f), Arg('>', f"{aln_f}.stats"))
+
+        program_cmd= f"{VG.vg_folder}/vg stats" if VG.vg_folder else "vg stats"
+
+        pdb.set_trace()
+        vg_runner = RunProgram(program=program_cmd,
+                               args=args)
+        
+        if verbose is True:
+            print("Command line is: {0}".format(vg_runner.cmd_line))
+
+        stdout, stderr, is_error = vg_runner.run_popen(raise_exc=False)
+
+        return f"{aln_f}.stats"
+
+    def run_augment(self, vg_f: str, aln_f: str, prefix: str, verbose: bool=True):
+        """
+        run vg augment
+
+        Parameters
+        ----------
+        vg_f : str
+               Path to graph.vg file
+        aln_f : str
+                Path to aln.gam file
+        prefix : str
+                 Output prefix
+        
+        Returns
+        -------
+        aug_graph_f : str
+                      Path to augmented.vg file
+        aug_aln_f : str
+                    Path to augmented.gam file
+        """
+        Arg = namedtuple('Argument', 'option value')
+        args = (Arg('', vg_f), Arg('', aln_f), Arg('-A', f"{prefix}.gam"), Arg('>', f"{prefix}.vg"))
+
+        program_cmd= f"{VG.vg_folder}/vg augment" if VG.vg_folder else "vg augment"
+
+        vg_runner = RunProgram(program=program_cmd,
+                               args=args)
+        
+        if verbose is True:
+            print("Command line is: {0}".format(vg_runner.cmd_line))
+
+        stdout, stderr, is_error = vg_runner.run_popen(raise_exc=False)
+
+        return f"{prefix}.vg", f"{prefix}.gam"
+
+
+
--- tests/test_VgToolkit.py
+++ tests/test_VgToolkit.py
@@ -1,17 +1,59 @@
 import pytest
+import pdb
+import os
+
+from GRAPH.VgToolkit import VG
 
 @pytest.fixture
 def vg_object(datadir, bcftools_folder):
     '''Returns a VG object'''
-
+    VG.vg_folder="/nfs/production/flicek/ensembl/variation/software//vg-v1.34.0/"
     vg_object = VG()
 
     return vg_object
 
-def test_run_giraffe(vg_object):
+def test_run_autoindex(vg_object, datadir):
+    '''
+    Test function to run 'vg autoindex'
+    '''
+
+    ofiles = vg_object.run_autoindex(ref=f"{datadir}/VG/hs37d5.fa",
+                                     vcf=f"{datadir}/VG/chr20.vcf.gz",
+                                     prefix=f"{datadir}/outdir/test.autoindex")
+    
+    for f in [f"{os.getcwd()}/data/outdir/test.autoindex.dist", f"{os.getcwd()}/data/outdir/test.autoindex.giraffe.gbz", 
+    f"{os.getcwd()}/data/outdir/test.autoindex.min"]:
+        assert f in ofiles
+
+def test_run_giraffe(vg_object, datadir):
     '''
     Test function to run 'vg giraffe' on a FASTQ file
     '''
-    pdb.set_trace()
+    
+    outfile = vg_object.run_giraffe(gbz_f=f"{datadir}/outdir/test.autoindex.giraffe.gbz",
+                                    min=f"{datadir}/outdir/test.autoindex.min", 
+                                    dist=f"{datadir}/outdir/test.autoindex.dist", 
+                                    fastq=f"{datadir}/VG/input.fq", 
+                                    prefix=f"{datadir}/outdir/test")
 
-    vg_object.run_giraffe()
+    assert os.path.isfile(outfile) is True
+
+def test_run_stats(vg_object, datadir):
+    '''
+    Test function to run 'vg stats' on a .gam file
+    '''
+    outfile = vg_object.run_stats(aln_f=f"{datadir}/outdir/test.gam")
+
+    assert os.path.isfile(outfile) is True
+
+def test_run_augment(vg_object, datadir):
+    '''
+    Test function to run 'vg augment' on a .gam file
+    '''
+    outfiles = vg_object.run_augment(vg_f=f"{datadir}/VG/x.vg",
+                                    aln_f=f"{datadir}/VG/aln.gam",
+                                    prefix=f"{datadir}/outdir/aug")
+
+    assert os.path.isfile(outfiles[0]) is True
+    assert os.path.isfile(outfiles[1]) is True
+
